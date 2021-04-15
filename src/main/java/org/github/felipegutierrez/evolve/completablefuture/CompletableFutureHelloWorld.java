5e751a456f626608657a02fc8d41484435ab4f87
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2021-04-15 09:46:10 +0200

    first completable future [skip ci]

---
+++ src/main/java/org/github/felipegutierrez/evolve/completablefuture/CompletableFutureHelloWorld.java
@@ -0,0 +1,22 @@
+package org.github.felipegutierrez.evolve.completablefuture;
+
+import org.github.felipegutierrez.evolve.service.HelloWorldService;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.github.felipegutierrez.evolve.util.CommonUtil.delay;
+import static org.github.felipegutierrez.evolve.util.LoggerUtil.log;
+
+public class CompletableFutureHelloWorld {
+
+    public static void main(String[] args) {
+        HelloWorldService helloWorldService = new HelloWorldService();
+
+        CompletableFuture.supplyAsync(() -> helloWorldService.helloWorld())
+                .thenAccept(result -> log("result is " + result))
+                .join();
+
+        log("Done!");
+        // delay(2000);
+    }
+}
